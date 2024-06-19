1cb3b9a6c61e25a6c305ab4fcee437f35d9443bd
Vishnu Preetham Reddy Dasari <vishnupreethamreddy@gmail.com>
2024-06-18 21:43:59 -0400

    Removed Auth Token

--- huggingface.py
+++ huggingface.py
@@ -1,7 +1,7 @@
 import requests
 
 API_URL = "https://api-inference.huggingface.co/models/sentence-transformers/all-MiniLM-L6-v2"
-headers = {"Authorization": "Bearer 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"}
+
 
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
